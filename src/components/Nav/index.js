09f67b372d5201051b610fdc60b901dec574fbe8
lorreina <slorreina369@gmail.com>
2022-09-09 10:16:02 -0700

    about page created

---
+++ src/components/Nav/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav(){
+    const categories = [
+        {
+            name:'Commercial',
+            description:
+            'Photos of grocery stores, foodtrucks, and other commercial projects'
+        },
+        {name: "Portraits", description:"Porttraits of people in my life"},
+        {name: "Food", description:"Delicious delicacies"},
+        {
+            name:"Landscape",
+            description:"Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
+
+    function handleClick(){
+        console.log("click handled")
+    }
+
+    return(
+        <header data-testid="header" className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera"> 📸</span>Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about" onClick={() => handleClick()}>
+                            About Me
+                        </a>
+                    </li>
+                    <li className={"mx-2"}>
+                        <span onClick={() => handleClick()}>
+                            Contact
+                        </span>
+                    </li>
+                    {categories.map((category) =>(
+                        <li className="mx-1" key={category.name}>
+                            <span onClick={() => {handleClick(); }}>
+                                {capitalizeFirstLetter(category.name)}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
